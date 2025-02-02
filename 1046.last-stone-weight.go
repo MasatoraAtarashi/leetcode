db15f825f5cba5a1a2160b1ad8ce7c83f4d3592b
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-02 23:36:33 +0900

    1046.last-stone-weigh

---
+++ 1046.last-stone-weight.go
@@ -0,0 +1,20 @@
+package main
+
+import "math"
+
+func lastStoneWeight(stones []int) int {
+	mh := MaxHeap{}
+	for _, stone := range stones {
+		mh.Push(stone)
+	}
+
+	for len(mh.data) > 1 {
+		x := mh.Pop()
+		y := mh.Pop()
+		if x != y {
+			mh.Push(x - y)
+		}
+	}
+
+	return mh.Peek()
+}
--- heap.go
+++ heap.go
@@ -78,3 +78,82 @@ func (m *MinHeap) swap(idx1, idx2 int) {
 	m.data[idx1] = m.data[idx2]
 	m.data[idx2] = tmp
 }
+
+type MaxHeap struct {
+	data []int
+}
+
+// Push	要素を追加し、ヒープ条件を維持
+func (m *MaxHeap) Push(val int) {
+	m.data = append(m.data, val)
+	m.heapifyUp(len(m.data) - 1)
+}
+
+// Pop	最大値（ルート）を削除し、ヒープを再構成
+func (m *MaxHeap) Pop() int {
+	if m.IsEmpty() {
+		return 0
+	}
+
+	min := m.data[0]
+	m.data[0] = m.data[len(m.data)-1]
+	m.data = m.data[:len(m.data)-1]
+	m.heapifyDown(0)
+	return min
+}
+
+// Peek	最大値を取得（削除しない）
+func (m *MaxHeap) Peek() int {
+	if m.IsEmpty() {
+		return 0
+	}
+
+	return m.data[0]
+}
+
+// Size	ヒープのサイズを取得
+func (m *MaxHeap) Size() int {
+	return len(m.data)
+}
+
+// IsEmpty ヒープが空かを確認
+func (m *MaxHeap) IsEmpty() bool {
+	return len(m.data) == 0
+}
+
+func (m *MaxHeap) heapifyUp(idx int) {
+	for idx > 0 {
+		parent := (idx - 1) / 2
+		if m.data[parent] >= m.data[idx] {
+			break
+		}
+		m.swap(parent, idx)
+		idx = parent
+	}
+}
+
+func (m *MaxHeap) heapifyDown(idx int) {
+	for {
+		leftChild := 2*idx + 1
+		rightChild := leftChild + 1
+		if leftChild >= len(m.data) {
+			break
+		}
+
+		biggest := leftChild
+		if rightChild < len(m.data) && m.data[rightChild] > m.data[biggest] {
+			biggest = rightChild
+		}
+		if m.data[idx] >= m.data[biggest] {
+			break
+		}
+		m.swap(idx, biggest)
+		idx = biggest
+	}
+}
+
+func (m *MaxHeap) swap(idx1, idx2 int) {
+	tmp := m.data[idx1]
+	m.data[idx1] = m.data[idx2]
+	m.data[idx2] = tmp
+}
