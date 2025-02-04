c0437d6e6c6857d61fa3e6960a0f189e52a71ec2
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-05 02:00:10 +0900

    238.product-of-array-except-self

---
+++ 238.product-of-array-except-self.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	leftMulti := make([]int, len(nums))
+	for i := range nums {
+		if i == 0 {
+			leftMulti[i] = 1
+			continue
+		}
+		if i == 1 {
+			leftMulti[i] = nums[i-1]
+			continue
+		}
+		leftMulti[i] = leftMulti[i-1] * nums[i-1]
+	}
+	// fmt.Println("leftMulti", leftMulti)
+
+	rightMulti := make([]int, len(nums))
+	result := make([]int, len(nums))
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i == len(nums)-1 {
+			rightMulti[i] = 1
+		} else if i == len(nums)-2 {
+			rightMulti[i] = nums[i+1]
+		} else {
+			rightMulti[i] = rightMulti[i+1] * nums[i+1]
+		}
+
+		// fmt.Println("rightMulti", rightMulti)
+		result[i] = leftMulti[i] * rightMulti[i]
+		// fmt.Println("result", result)
+	}
+	return result
+}
