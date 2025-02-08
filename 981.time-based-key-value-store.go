2f3886413e862723960dbb490d326ff6bf777ccc
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-08 18:26:06 +0900

    981.time-based-key-value-store

---
+++ 981.time-based-key-value-store.go
@@ -0,0 +1,44 @@
+package main
+
+type TimeMap struct {
+	m map[string][]TsValue
+}
+
+type TsValue struct {
+	timestamp int
+	value     string
+}
+
+func Constructor() TimeMap {
+	return TimeMap{
+		m: map[string][]TsValue{},
+	}
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.m[key] = append(this.m[key], TsValue{
+		timestamp: timestamp,
+		value:     value,
+	})
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	vs := this.m[key]
+	i := 0
+	j := len(vs) - 1
+	val := ""
+	for i <= j {
+		mid := (i + j) / 2
+		if vs[mid].timestamp == timestamp {
+			val = vs[mid].value
+			break
+		} else if vs[mid].timestamp < timestamp {
+			val = vs[mid].value
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+	}
+
+	return val
+}
