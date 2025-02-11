2a6c94c05d608e889701ea28a581b5dc678e8b55
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-11 19:44:21 +0900

    424 improve performance

--- 424.longest-repeating-character-replacement.go
+++ 424.longest-repeating-character-replacement.go
@@ -1,29 +1,25 @@
 package main
 
 func characterReplacement(s string, k int) int {
-	var start, end, max int
-	for end <= len(s)-1 {
-		maxFreq := 0
-		freq := make([]int, 26)
-		for i := start; i <= end; i++ {
-			freq[s[i]-'A']++
-			if maxFreq < freq[s[i]-'A'] {
-				maxFreq = freq[s[i]-'A']
-			}
+	freq := make([]int, 26)
+	var start, max, maxFreq int
+	for end := 0; end < len(s); end++ {
+		freq[s[end]-'A']++
+		if freq[s[end]-'A'] > maxFreq {
+			maxFreq = freq[s[end]-'A']
 		}
-
 		windowLen := end - start + 1
 		// kで置き換えられなかったら窓を左から縮める
 		if windowLen-maxFreq > k {
+			freq[s[start]-'A']--
 			start++
-		} else {
-			// k回で置き換えられるなら、窓の幅が現状の最大。更新して窓を広げる
-			if max < windowLen {
-				max = windowLen
-			}
-			end++
+			continue
 		}
 
+		// k回で置き換えられるなら、窓の幅が現状の最大。更新して窓を広げる
+		if max < windowLen {
+			max = windowLen
+		}
 	}
 
 	return max
