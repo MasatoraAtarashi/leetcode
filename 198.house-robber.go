5b70ae35963a7a8498524c861f87c3a4dbed42d0
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-19 00:56:32 +0900

    198.house-robber

--- 198.house-robber.go
+++ 198.house-robber.go
@@ -1,33 +1,25 @@
 package main
 
 func rob(nums []int) int {
-	var robrob func(nums []int) int
-	robrob = func(nums []int) int {
-		if len(nums) == 0 {
-			return 0
-		}
-		if len(nums) == 1 {
-			return nums[0]
-		}
-		if len(nums) == 2 {
-			return maxx(nums[0], nums[1])
-		}
-
-		if max[len(nums)-1] != 0 {
-			return max[len(nums)-1]
-		}
-
-		p1 := robrob(nums[:len(nums)-2]) + nums[len(nums)-1]
-		p2 := robrob(nums[:len(nums)-1])
-		if p1 < p2 {
-			max[len(nums)-1] = p2
-			return p2
-		}
-		max[len(nums)-1] = p1
-		return p1
+	if len(nums) == 0 {
+		return 0
 	}
-
-	return robrob(nums)
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return maxint(nums[0], nums[1])
+	}
+	prevprev := nums[0]
+	prev := nums[1]
+	var current int
+	for i := 2; i < len(nums); i++ {
+		current = nums[i]
+		tmpprev := prev
+		prev = maxint(prevprev+current, prev)
+		prevprev = maxint(prevprev, tmpprev)
+	}
+	return prev
 }
 
 func maxint(a, b int) int {
