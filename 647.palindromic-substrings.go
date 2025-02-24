5c9c2ba69448ff90209724858a6d730328d2eb38
Masatora Atarashi <atarashi.masatora@gmail.com>
2025-02-24 23:59:57 +0900

    647: runtime: 754ms -> 33ms

--- 647.palindromic-substrings.go
+++ 647.palindromic-substrings.go
@@ -3,25 +3,60 @@ package main
 import "fmt"
 
 func countSubstrings(s string) int {
+	// 0: default, 1: palindromic, -1: not
+	memo := make([][]int, len(s))
+
 	var isPali func(i, j int) bool
 	isPali = func(i, j int) bool {
 		fmt.Println("call")
+		fmt.Println(i, j)
+
+		if i < 0 || j >= len(s) || i > j {
+			return false
+		}
+
+		if memo[i] == nil {
+			memo[i] = make([]int, len(s))
+		}
+
+		if memo[i][j] != 0 {
+			if memo[i][j] == 1 {
+				return true
+			} else {
+				return false
+			}
+		}
+
 		// 1文字
 		if i == j {
+			memo[i][j] = 1
 			return true
 		}
 
 		// 2文字
 		if i+1 == j {
-			return s[i] == s[j]
+			if s[i] == s[j] {
+				memo[i][j] = 1
+				return true
+			} else {
+				memo[i][j] = -1
+				return false
+			}
 		}
 
 		// 3文字以上
 		if s[i] != s[j] {
+			memo[i][j] = -1
 			return false
 		}
 
-		return isPali(i+1, j-1)
+		if isPali(i+1, j-1) {
+			memo[i][j] = 1
+			return true
+		}
+
+		memo[i][j] = -1
+		return false
 	}
 
 	var count int
